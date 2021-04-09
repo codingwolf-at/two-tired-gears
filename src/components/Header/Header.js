d6cca545b61952ab04fc6f54a59b00205feb3e10
codingwolf-at <atameshwari9@gmail.com>
2021-04-09 23:57:15 +0530

    feat: cart and wishlist counter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,15 @@ import "./header.css";
 import siteLogo from "./img/fire-wheel.png";
 
 export const Header = () => {
-  const { dispatch } = useCart();
+  const { dispatch, itemsInCart, wishList } = useCart();
   return (
     <nav className="nav nav-dark">
       <img src={siteLogo} alt="site-logo"/>
       <span class="nav-brand">Two Tired Gears</span>
       <div class="nav-link-container">
         <span onClick={() => dispatch({type: "ROUTE", payload: "products"})} className="nav-link">PRODUCTS</span>
-        <span onClick={() => dispatch({type: "ROUTE", payload: "cart"})} className="nav-link">CART</span>
-        <span onClick={() => dispatch({type: "ROUTE", payload: "wishlist"})}  className="nav-link">WISHLIST</span>
+        <span onClick={() => dispatch({type: "ROUTE", payload: "cart"})} className="nav-link">CART | {itemsInCart.length}</span>
+        <span onClick={() => dispatch({type: "ROUTE", payload: "wishlist"})}  className="nav-link">WISHLIST | {wishList.length}</span>
       </div>
     </nav>
   )
