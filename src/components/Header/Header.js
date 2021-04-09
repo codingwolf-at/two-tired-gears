6f3a3d8d73a6e6304da9f6e7e9240cf9f63e30ac
codingwolf-at <atameshwari9@gmail.com>
2021-04-09 14:53:27 +0530

    added: dispatch actions to nav-links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
+import { useCart } from "../../context/cart-context";
 import "./header.css";
 import siteLogo from "./img/fire-wheel.png";
 
-const Header = () => {
+export const Header = () => {
+  const { dispatch } = useCart();
   return (
     <nav className="nav nav-dark">
       <img src={siteLogo} alt="site-logo"/>
       <span class="nav-brand">Two Tired Gears</span>
       <div class="nav-link-container">
-        <span className="nav-link">CART</span>
-        <span className="nav-link">WISHLIST</span>
+        <span onClick={() => dispatch({type: "ROUTE", payload: "products"})} className="nav-link">PRODUCTS</span>
+        <span onClick={() => dispatch({type: "ROUTE", payload: "cart"})} className="nav-link">CART</span>
+        <span onClick={() => dispatch({type: "ROUTE", payload: "wishlist"})}  className="nav-link">WISHLIST</span>
       </div>
     </nav>
   )
-}
-
-export default Header;
+};
