55364fc85f129d6161c02fb835ef234c2ae9a802
codingwolf-at <atameshwari9@gmail.com>
2021-04-09 13:59:57 +0530

    added: site logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import "./header.css";
+import siteLogo from "./img/fire-wheel.png";
 
 const Header = () => {
   return (
     <nav className="nav nav-dark">
+      <img src={siteLogo} alt="site-logo"/>
       <span class="nav-brand">Two Tired Gears</span>
       <div class="nav-link-container">
         <span className="nav-link">CART</span>
