38b0f31565b8607c3cc2926a601258515d355d11
codingwolf-at <atameshwari9@gmail.com>
2021-04-15 01:11:16 +0530

    added: activeStyle property to NavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,14 +5,19 @@ import siteLogo from "./img/fire-wheel.png";
 
 export const Header = () => {
   const { itemsInCart, wishList } = useCart();
+
+  const activeStyle = {
+    fontWeight: "bold"
+  }
+
   return (
     <nav className="nav nav-dark">
       <img src={siteLogo} alt="site-logo"/>
       <span class="nav-brand">Two Tired Gears</span>
       <div class="nav-link-container">
-        <NavLink className="nav-link" to="/" >Products</NavLink>
-        <NavLink className="nav-link" to="/cart" >Cart | {itemsInCart.length}</NavLink>
-        <NavLink className="nav-link" to="/wishlist" >Wishlist | {wishList.length}</NavLink>
+        <NavLink className="nav-link" to="/" activeStyle={activeStyle} >Products</NavLink>
+        <NavLink className="nav-link" to="/cart" activeStyle={activeStyle} >Cart | {itemsInCart.length}</NavLink>
+        <NavLink className="nav-link" to="/wishlist" activeStyle={activeStyle} >Wishlist | {wishList.length}</NavLink>
       </div>
     </nav>
   )
