79ac22313822b7f84a8f27f6d92fc23cffd05c82
codingwolf-at <atameshwari9@gmail.com>
2021-04-14 22:00:12 +0530

    testing: sorting

--- src/components/ProductListing/ProductListing.js
+++ src/components/ProductListing/ProductListing.js
@@ -1,10 +1,50 @@
+import { useReducer } from "react";
 import { useCart } from "../../context/cart-context";
 import "./product-listing.css";
 
 export const ProductListing = () => {
-  const {products, dispatch} = useCart();
+
+  const {products, dispatch: cartDispatch } = useCart();
+
+  // const reducer = (state, action) => {
+  //   switch (action.type) {
+  //     case "SORT":
+  //       return {
+  //         ...state,
+  //         sortBy: action.payload
+  //       };
+  //     default: return state;
+  // }
+
+  // const [{ sortBy }, dispatch] = useReducer(reducer, { sortBy: null })
+
   return (
     <div>
+      {/* <fieldset>
+        <legend>Sort By</legend>
+        <label>
+          <input
+            type="radio"
+            name="sort"
+            onClick={() =>
+              dispatch({ type: "SORT", payload: "PRICE_LOW_TO_HIGH" })
+            }
+            checked={sortBy && sortBy === "PRICE_LOW_TO_HIGH"}
+          ></input>{" "}
+          Price - Low to High
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="sort"
+            onClick={() =>
+              dispatch({ type: "SORT", payload: "PRICE_HIGH_TO_LOW" })
+            }
+            checked={sortBy && sortBy === "PRICE_HIGH_TO_LOW"}
+          ></input>{" "}
+          Price - High to Low
+        </label>
+      </fieldset> */}
       <main className="product-listing">
         {
           products.map(product => (
@@ -15,8 +55,8 @@ export const ProductListing = () => {
                     <h2>{product.name}</h2>
                     <h4>Price: {product.price}/-</h4>
                   </div>
-                  <button onClick={() => dispatch({type: "ADD_TO_CART", payload: product})} class="btn btn-primary btn-block">ADD TO CART</button>
-                  <button onClick={() => dispatch({type: "ADD_TO_WISHLIST", payload: product})} class="btn btn-secondary btn-block">ADD TO WISHLIST</button>
+                  <button onClick={() => cartDispatch({type: "ADD_TO_CART", payload: product})} class="btn btn-primary btn-block">ADD TO CART</button>
+                  <button onClick={() => cartDispatch({type: "ADD_TO_WISHLIST", payload: product})} class="btn btn-secondary btn-block">ADD TO WISHLIST</button>
               </div>
             </div>
           ))
